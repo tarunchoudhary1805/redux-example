471ae8ec747d3f45a72d159ca61d03fa7fa924c1
Tarun <tarunchoudhary1805@gmail.com>
2020-08-13 14:58:08 +0530

    First commit with cake shop container

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import  CakeContainer  from "./components/CakeContainer";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+
+
+function App() {
+  return (
+    <Provider store={store}>
+      <div className="App">
+        <CakeContainer />
+    
+      </div>
+    </Provider>
+  );
+}
+
+export default App;
